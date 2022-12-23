c6b44eb3f184fd8d2d408b0415d570977cf7d73a
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-23 10:43:23 +0100

    InitScrap

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from selectorlib import Extractor
+
+URL="https://mepel.pl/gry-planszowe"
+#+ /2
+
+URL2="https://shopgracz.pl/19-gry-planszowe"
+#+ ?page=2
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def scrape(url):
+    response=requests.get(url, headers=HEADERS)
+    source = response.text
+
+    return source
+
+def extract(source):
+    extractor=Extractor.from_yaml_file("extract.yaml")
+    value=extractor.extract(source)["Mepel"]
+    return value
+if __name__ == '__main__':
+    print(scrape(URL))
+    #print(extract(scrape(URL)))
+    #print(extract(scrape(URL2)))
+
+    print(extract(scrape(URL)))
+
+
+
+
