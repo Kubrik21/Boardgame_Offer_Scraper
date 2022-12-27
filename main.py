d18d57749da74234dba5a54213b9dbefade202c3
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-27 13:29:56 +0100

    Wip

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ import scrapShopgracz
 
 if __name__ == '__main__':
     # Scrap and saved in file
-    L2=scrapShopgracz.scrap()
-    with open("Shopgracz","w",encoding='utf-8') as file:
-        json.dump(L2,file, ensure_ascii=False)
+    #L2=scrapShopgracz.scrap()
+    #with open("Shopgracz","w",encoding='utf-8') as file:
+      #  json.dump(L2,file, ensure_ascii=False)
     # Scrap and saved in file
-    L1=scrapMapel.scrap()
-    with open("Mepel","w",encoding='utf-8') as file:
-        json.dump(L1,file,ensure_ascii=False)
+    #L1=scrapMapel.scrap()
+    #with open("Mepel","w",encoding='utf-8') as file:
+     #   json.dump(L1,file,ensure_ascii=False)
 
     with open("Mepel", "r", encoding='utf-8') as file:
         L1=file.read()
     with open("Shopgracz", "r", encoding='utf-8') as file:
         L2=file.read()
 
-    #print(L2)
-    All=merge(L1,L2)
+   # print(json.loads(L2)[1])
+    All=merge(json.loads(L1),json.loads(L2))
     for x in range(len(All)):
         print(All[x])
 
