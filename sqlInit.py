23a4f629e34d89922341a4aa47fa3974b8d799ae
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-29 15:41:21 +0100

    Creating tables if not exist

--- sqlInit.py
+++ sqlInit.py
@@ -8,7 +8,7 @@ def init():
         user="root",
         password="123MySql321")
 
-        mycursor = mydb.cursor()
+        mycursor = mydb.cursor(buffered=True)
 
     except mysql.connector.Error as err:
         print("Connection error. Check login data")
@@ -22,12 +22,30 @@ def init():
             password="123MySql321",
             database="dbo_boardgames"
         )
-        mycursor = mydb.cursor()
+        mycursor = mydb.cursor(buffered=True)
         print("Connection completed. DatabaseObject exist")
 
     except mysql.connector.Error as err:
         print("DatabaseObject doesn't exist. Creating DatabaseObject and reconnecting")
         mycursor.execute("CREATE DATABASE dbo_boardgames")
+
+        # Drop and Create
+
+        mycursor.execute("DROP TABLE IF EXISTS db_offer")
+        mycursor.execute("DROP TABLE IF EXISTS db_boardgames")
+        mycursor.execute("DROP TABLE IF EXISTS db_date")
+
+        # DB_Date
+        mycursor.execute("CREATE TABLE db_date (id INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(255))")
+
+        # DB_Boardgames
+        mycursor.execute(
+            "CREATE TABLE db_boardgames (id INT AUTO_INCREMENT PRIMARY KEY, boardgame_name VARCHAR(255), img VARCHAR(255))")
+
+        # DB_Offer
+        mycursor.execute(
+            "CREATE TABLE db_offer (id INT AUTO_INCREMENT PRIMARY KEY, store VARCHAR(255), link VARCHAR(255), price FLOAT(7,2), date_id INT, game_id INT, FOREIGN KEY (date_id) REFERENCES db_date(id), FOREIGN KEY (game_id) REFERENCES db_boardgames(id))")
+
         init()
 
     return(mycursor)
