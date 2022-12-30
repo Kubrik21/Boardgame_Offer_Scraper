b44f5d2a0b1422a512f56da49ca3548b9e9423e0
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-30 11:10:04 +0100

    cleaned main

--- main.py
+++ main.py
@@ -19,32 +19,7 @@ if __name__ == '__main__':
     #pobierz dane z BD z ostatniej aktualizacji i zmerguj je do prezentacji
 
 
-    #Scrap and merge
-    #Then actualize
 
-    #L2=scrapShopgracz.scrap()
-    #with open("Shopgracz","w",encoding='utf-8') as file:
-    #  json.dump(L2,file, ensure_ascii=False)
-
-    L1=scrapMapel.scrap()
-    with open("Mepel","w",encoding='utf-8') as file:
-      json.dump(L1,file,ensure_ascii=False)
-
-    with open("Mepel", "r", encoding='utf-8') as file:
-        L1=file.read()
-    with open("Shopgracz", "r", encoding='utf-8') as file:
-        L2=file.read()
-
-   # print(json.loads(L2)[1])
-    #Merging data - 350 games available on both sites
-    All=merge(json.loads(L1),json.loads(L2))
-    wyn=0
-    for x in range(len(All)):
-        wyn+=1
-        #print(All[x])
-        if len(All[x]['Shop'])!=1:
-            wyn+=1
-    #print(wyn)
 
 
 
