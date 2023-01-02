99941cc65576f2f107df08276a3bb88c8f0e4f9b
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2023-01-02 13:30:55 +0100

    Statistic rdy for frontend

--- pages/statistic.py
+++ pages/statistic.py
@@ -1,8 +1,17 @@
-
+import sqlOperations as db
 #Get whole gamelist Titles
 #Shows search bar to choose the game
 
+def main():
+
+    List= db.get_game_index()
+
+    #Elem to wybór dowolnej gry- indeks z listy
+    elem=2
 #then Q to DB for data from last 7 days
+    statisticList=db.game_stats(elem)
 #create plotly
+
+
 
 
